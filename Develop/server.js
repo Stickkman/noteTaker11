4a5b6dfa44530c6a958cf17e3f4971a21d96e573
stickkman <stickkman@gmail.com>
2023-01-31 16:06:41 -0500

    created GET api/notes route to read and parse db notes

--- Develop/server.js
+++ Develop/server.js
@@ -15,13 +15,16 @@ app.use(express.static('public'));              // root dir for static files
 
 //HTML routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html'))); // '/notes' returns 'notes.html'
-//Api routes
-app.get(('/api/notes', (req, res) => {
+//API routes
+app.get('/api/notes', (req, res) => {
+    const rawNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8'); //reads db.json and sets contents to rawNotes 
+    const parsedNotes = JSON.parse(rawNotes); res.json(parsedNotes); // parses rawNotes and sends response with that parsed data
+});
 
-})
 
 
 
+// v-- placed as last route to prevent issues w running before other routes
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));      // fallback route to 'index.html'
 
 
